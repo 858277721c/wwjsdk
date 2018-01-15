ec89ab2b4f21063b7eda22f3da00b3756b8df095
zjun <565061763@qq.com>
2018-01-15 16:06:30 +0800

    新增log管理

---
+++ lib/src/main/java/com/fanwe/lib/wwjsdk/log/DefaultFileHandler.java
@@ -0,0 +1,27 @@
+package com.fanwe.lib.wwjsdk.log;
+
+import android.content.Context;
+
+import com.fanwe.lib.log.FFileHandler;
+
+import java.io.IOException;
+
+/**
+ * 默认的log文件保存处理类
+ */
+public class DefaultFileHandler extends FFileHandler
+{
+    public DefaultFileHandler(Context context) throws IOException
+    {
+        super(FFileHandler.getLogFilePath("wwserver", context),
+                100 * MB,
+                1,
+                true);
+    }
+
+    public void addToLogger()
+    {
+        WWLogger.get().removeHandler(this);
+        WWLogger.get().addHandler(this);
+    }
+}
---
+++ lib/src/main/java/com/fanwe/lib/wwjsdk/sdk/WWSDKManager.java
@@ -0,0 +1,73 @@
+package com.fanwe.lib.wwjsdk.sdk;
+
+import android.content.Context;
+
+import com.fanwe.lib.wwjsdk.log.DefaultFileHandler;
+import com.fanwe.lib.wwjsdk.log.WWLogger;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+/**
+ * 娃娃sdk管理类
+ */
+public class WWSDKManager
+{
+    private static WWSDKManager sInstance;
+    private Context mContext;
+    private DefaultFileHandler mFileHandler;
+
+    private WWSDKManager()
+    {
+    }
+
+    public static WWSDKManager getInstance()
+    {
+        if (sInstance == null)
+        {
+            synchronized (WWSDKManager.class)
+            {
+                if (sInstance == null)
+                {
+                    sInstance = new WWSDKManager();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    /**
+     * 初始化娃娃sdk
+     *
+     * @param context
+     */
+    public final void init(Context context)
+    {
+        if (mContext == null)
+        {
+            mContext = context.getApplicationContext();
+
+            DefaultFileHandler fileHandler = getFileHandler();
+            if (fileHandler != null)
+            {
+                fileHandler.addToLogger();
+            }
+        }
+    }
+
+    private DefaultFileHandler getFileHandler()
+    {
+        if (mFileHandler == null)
+        {
+            try
+            {
+                mFileHandler = new DefaultFileHandler(mContext);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+                WWLogger.get().log(Level.SEVERE, e.toString(), e);
+            }
+        }
+        return mFileHandler;
+    }
+}
