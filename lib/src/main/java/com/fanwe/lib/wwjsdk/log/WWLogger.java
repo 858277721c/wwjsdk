d905d5064cd8f082134728d33d7c1572075f2b76
zjun <565061763@qq.com>
2018-01-15 12:06:30 +0800

    log:1.0.1

--- lib/src/main/java/com/fanwe/lib/wwjsdk/log/WWLogger.java
+++ lib/src/main/java/com/fanwe/lib/wwjsdk/log/WWLogger.java
@@ -1,16 +1,16 @@
 package com.fanwe.lib.wwjsdk.log;
 
+import com.fanwe.lib.log.FLogger;
+
 import java.util.logging.Logger;
 
 /**
  * Created by Administrator on 2017/12/11.
  */
-public class WWLogger
+public class WWLogger extends FLogger
 {
-    private static final String TAG_DEFAULT = "WWServer";
-
     public static Logger get()
     {
-        return Logger.getLogger(TAG_DEFAULT);
+        return get("WWServer");
     }
 }
