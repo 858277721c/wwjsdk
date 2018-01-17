6d3d3205467b62e1bcb0c66bc3bd9e91175e23cf
zjun <565061763@qq.com>
2018-01-17 10:33:30 +0800

    重构

--- lib/src/main/java/com/fanwe/lib/wwjsdk/sdk/proxy/WWControlSDKProxy.java
+++ lib/src/main/java/com/fanwe/lib/wwjsdk/sdk/proxy/WWControlSDKProxy.java
@@ -18,7 +18,7 @@ import java.util.logging.Level;
  */
 public abstract class WWControlSDKProxy implements IWWControlSDKProxy
 {
-    public WWControlSDKProxy()
+    protected WWControlSDKProxy()
     {
 
     }
