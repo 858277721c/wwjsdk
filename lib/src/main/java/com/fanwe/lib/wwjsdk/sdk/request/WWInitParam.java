e23a9f517534cbe3694c086a1df7824b3ceb6b5a
zjun <565061763@qq.com>
2018-01-18 11:02:11 +0800

    重构

--- lib/src/main/java/com/fanwe/lib/wwjsdk/sdk/request/WWInitParam.java
+++ lib/src/main/java/com/fanwe/lib/wwjsdk/sdk/request/WWInitParam.java
@@ -13,4 +13,17 @@ public class WWInitParam
      * 设置本局游戏下爪后，是否保持足够的爪力把娃娃抓起(1-保持爪力，如果为1的话，其他控制爪力的参数无效；0-不保持)，默认不保持
      */
     public int keepCatch = 0;
+
+    /**
+     * 抓起爪力[0-100]，默认值0表示由娃娃机内部控制
+     */
+    public int clawForceStart = 0;
+    /**
+     * 到顶爪力[0-100]，默认值0表示由娃娃机内部控制
+     */
+    public int clawForceTop = 0;
+    /**
+     * 移动爪力[0-100]，默认值0表示由娃娃机内部控制
+     */
+    public int clawForceMove = 0;
 }
